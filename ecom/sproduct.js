607201d4f911a4c9cfe61d6397c102aacafed11a
RjMungra <109795943+RjMungra@users.noreply.github.com>
2022-07-22 16:22:49 +0530

    Add files via upload

---
+++ ecom/sproduct.js
@@ -0,0 +1,18 @@
+
+	var mainImg = document.getElementById("mainImg");
+	var smallimg = document.getElementsByClsssName("small-img");
+
+	smallimg[0].onclick = function(){
+		mainImg.src = smallimg[0].src;
+	}
+	smallimg[1].onclick = function(){
+		mainImg.src = smallimg[1].src;
+	}
+	smallimg[2].onclick = function(){
+		mainImg.src = smallimg[2].src;
+	}
+	smallimg[3].onclick = function(){
+		minainImg.src = smallimg[3].src;
+	}
+	
+
